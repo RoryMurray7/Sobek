be45e9bdd3b800bb893a496ca4b48b9b78c78861
RoryMurray2 <24rmurray@liverpoolmathsschool.org>
2025-03-06 10:15:21 +0000

    many improvements to data analysis section

--- src/Analysis.tsx
+++ src/Analysis.tsx
@@ -1,5 +1,6 @@
-import Orb from "./components/Orb/Orb";
+import LetterGlitch from "./components/LetterGlitch/LetterGlitch";
 import "./index.css";
+import microchip from "./assets/microchip.png";
 
 function Analysis() {
   return (
@@ -7,14 +8,20 @@ function Analysis() {
       <div className="analysisTitle">
         <h1>Data Analysis</h1>
       </div>
-      <div className="orb">
-        <Orb
-          hoverIntensity={0.5}
-          rotateOnHover={true}
-          hue={210}
-          forceHoverState={false}
-        />
-      </div>
+      <div className="analysisIcon">
+      <img src={microchip} alt="microchip" className="microchip" />
+        <div className="letterGlitchContainer">
+          <div className="letterGlitch">
+            <LetterGlitch
+            glitchColors={["#FF0000", "#00FF00", "#0000FF"]}
+            glitchSpeed={0.1}
+            centerVignette={true}
+            outerVignette={false}
+            smooth={true}
+            />
+          </div>
+        </div>
+        </div>
     </div>
   );
 }
