3b8b098972553818a1645837a081a25f373d664e
RoryMurray7 <24rmurray@liverpoolmathsschool.org>
2025-03-06 00:29:05 +0000

    position orb

--- src/Analysis.tsx
+++ src/Analysis.tsx
@@ -7,7 +7,7 @@ function Analysis() {
       <div className="analysisTitle">
         <h1>Data Analysis</h1>
       </div>
-      <div style={{ width: "100%", height: "600px", position: "relative" }}>
+      <div className="orb">
         <Orb
           hoverIntensity={0.5}
           rotateOnHover={true}
