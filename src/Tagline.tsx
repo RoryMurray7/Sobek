8bce10c41a735a672e6916d123b8dc4a05285b0b
RoryMurray7 <24rmurray@liverpoolmathsschool.org>
2025-03-08 23:16:24 +0000

    small changes to index.css

--- src/Tagline.tsx
+++ src/Tagline.tsx
@@ -4,11 +4,11 @@ import "./index.css";
 function Tagline() {
   return (
     <div className="sectionContainer" style={{ backgroundColor: "#B0B0B0" }}>
-      <div className="leftHeader">
+      <div className="leftHeader" id="overviewHeader">
         <h1>See the Unseen</h1>
       </div>
 
-      <div className="overviewImage">
+      <div className="taglineImage">
         <img src={armyDesert} alt="Image of soldiers in the desert" />
       </div>
     </div>
