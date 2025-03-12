e11f2f1132e736206163531d31b9bb8838809319
RoryMurray7 <24rmurray@liverpoolmathsschool.org>
2025-03-12 10:24:54 +0000

    Remove animation, added pricing section, spotlight effect and began filling the pricing cards

--- src/Packages.tsx
+++ src/Packages.tsx
@@ -5,22 +5,23 @@ function Packages() {
   return (
     <div
       className="sectionContainer"
-      id="overviewContainer"
+      id="pricingContainer"
     >
+      <div className="large" id="overviewHeader">
+        <h1>Packages</h1>
+      </div>
       <div className="pricingCardContainer">
-        <div className="large" id="overviewHeader">
-          <h1>Packages</h1>
-        </div>
-        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.2)">
-            <p>hey</p>
+        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
+            <h5 className="pricingText">Baseline Intelligence</h5>
 
         </SpotlightCard>
-        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.2)">
-            <p>hey</p>
+        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
+            <h5 className="pricingText">Advanced Intelligence</h5>
 
         </SpotlightCard>
-        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.2)">
-            <p>hey</p>
+        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
+            <h5 className="pricingText">Elite & Covert Operations Support</h5>
+            <h2 className="pricingText">Contact Us</h2>
 
         </SpotlightCard>
       </div>
