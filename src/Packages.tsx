e73210bfff8aa42447d49918e0ac1dc8e92a628e
RoryMurray7 <24rmurray@liverpoolmathsschool.org>
2025-03-12 10:54:15 +0000

    fix package card headers

--- src/Packages.tsx
+++ src/Packages.tsx
@@ -5,23 +5,32 @@ function Packages() {
   return (
     <div
       className="sectionContainer"
-      id="pricingContainer"
+      id="packagesContainer"
     >
       <div className="large" id="overviewHeader">
         <h1>Packages</h1>
       </div>
-      <div className="pricingCardContainer">
+      <div className="packagesCardContainer">
         <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
-            <h5 className="pricingText">Baseline Intelligence</h5>
+            <div className="packagesCardHeader">
+              <h5 className="packagesText">Baseline Intelligence</h5>
+            </div>
+            <hr></hr>
+            <h6>test</h6>
 
         </SpotlightCard>
         <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
-            <h5 className="pricingText">Advanced Intelligence</h5>
+            <div className="packagesCardHeader">
+              <h5 className="packagesText">Advanced Intelligence</h5>
+            </div>
+            <hr></hr>
 
         </SpotlightCard>
         <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
-            <h5 className="pricingText">Elite & Covert Operations Support</h5>
-            <h2 className="pricingText">Contact Us</h2>
+            <div className="packagesCardHeader">
+              <h5 className="packagesText">Elite & Covert Operations Support</h5>
+            </div>
+            <hr></hr>
 
         </SpotlightCard>
       </div>
