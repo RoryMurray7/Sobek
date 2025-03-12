66dbaa56d8d8b85b38a18fe501f2ff2e0cd4bc7f
RoryMurray7 <24rmurray@liverpoolmathsschool.org>
2025-03-12 22:45:20 +0000

    add button

--- src/Packages.tsx
+++ src/Packages.tsx
@@ -3,67 +3,76 @@ import SpotlightCard from "./components/SpotlightCard/SpotlightCard";
 
 function Packages() {
   return (
-    <div
-      className="sectionContainer"
-      id="packagesContainer"
-    >
+    <div className="sectionContainer" id="packagesContainer">
       <div className="large" id="overviewHeader">
         <h1>Packages</h1>
       </div>
       <div className="packagesCardContainer">
-        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
-            <div className="packagesCardHeader">
-              <h5 className="packagesText">Baseline Intelligence</h5>
-            </div>
-            <hr></hr>
-            <ul className="included">
-              <li>High level threat assessments</li>
-              <li>Refined publicly available intelligence reports</li>
-              <li>Basic access to secure communication</li>
-            </ul>
-            <ul className="notIncluded">
-              <li>Direct operational support</li>
-              <li>Classified or proprietary intelligence</li>
-              <li>Custom or tailored surveillance</li>
-            </ul>
-
+        <SpotlightCard
+          className="custom-spotlight-card"
+          spotlightColor="rgba(77, 77, 77, 0.5)"
+        >
+          <div className="packagesCardHeader">
+            <h5 className="packagesText">Baseline Intelligence</h5>
+          </div>
+          <hr></hr>
+          <ul className="included">
+            <li>High level threat assessments</li>
+            <li>Refined publicly available intelligence reports</li>
+            <li>Basic access to secure communication</li>
+          </ul>
+          <ul className="notIncluded">
+            <li>Direct operational support</li>
+            <li>Classified or proprietary intelligence</li>
+            <li>Custom or tailored surveillance</li>
+          </ul>
         </SpotlightCard>
-        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
-            <div className="packagesCardHeader">
-              <h5 className="packagesText">Advanced Intelligence</h5>
+        <SpotlightCard
+          className="custom-spotlight-card"
+          spotlightColor="rgba(77, 77, 77, 0.5)"
+        >
+          <div className="packagesCardHeader">
+            <h5 className="packagesText">Advanced Intelligence</h5>
+          </div>
+          <hr></hr>
+          <ul className="included">
+            <li>Real-time situational intelligence</li>
+            <li>Access to restricted-source intelligence</li>
+            <li>Bespoke risk mitigation reports</li>
+            <li>Counterintelligence assessments</li>
+            <li>Limited operational advisories</li>
+          </ul>
+          <ul className="notIncluded">
+            <li>Full-spectrum asset deployment</li>
+            <li>Direct kinetic support</li>
+            <li>Unrestricted access to classified intelligence</li>
+          </ul>
+          <div className="buttonContainer">
+            <div className="contactUsButton">
+              <p>Contact Us</p>
             </div>
-            <hr></hr>
-            <ul className="included">
-              <li>Real-time situational intelligence</li>
-              <li>Access to restricted-source intelligence</li>
-              <li>Bespoke risk mitigation reports</li>
-              <li>Counterintelligence assessments</li>
-              <li>Limited operational advisories</li>
-            </ul>
-            <ul className="notIncluded">
-              <li>Full-spectrum asset deployment</li>
-              <li>Direct kinetic support</li>
-              <li>Unrestricted access to classified intelligence</li>
-            </ul>
-
+          </div>
         </SpotlightCard>
-        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
-            <div className="packagesCardHeader">
-              <h5 className="packagesText">Elite & Covert Operations Support</h5>
-            </div>
-            <hr></hr>
-            <ul className="included">
-              <li>Covert operational intelligence</li>
-              <li>Classified strategic briefings</li>
-              <li>Advanced counterintelligence and cybersecurity assessments</li>
-              <li>Direct liaison with specialized assets</li>
-              <li>Deep reconnaissance and threat analysis</li>
-            </ul>
-            <ul className="notIncluded">
-              <li>Public acknowledgment or transparency</li>
-              <li>Unlimited operational intervention</li>
-              <li>Any records of engagement</li>
-            </ul>
+        <SpotlightCard
+          className="custom-spotlight-card"
+          spotlightColor="rgba(77, 77, 77, 0.5)"
+        >
+          <div className="packagesCardHeader">
+            <h5 className="packagesText">Elite & Covert Operations Support</h5>
+          </div>
+          <hr></hr>
+          <ul className="included">
+            <li>Covert operational intelligence</li>
+            <li>Classified strategic briefings</li>
+            <li>Advanced counterintelligence and cybersecurity assessments</li>
+            <li>Direct liaison with specialized assets</li>
+            <li>Deep reconnaissance and threat analysis</li>
+          </ul>
+          <ul className="notIncluded">
+            <li>Public acknowledgment or transparency</li>
+            <li>Unlimited operational intervention</li>
+            <li>Any records of engagement</li>
+          </ul>
         </SpotlightCard>
       </div>
     </div>
