8ceb297aa5514ac63458766d0d57ff5cd71c923a
RoryMurray7 <24rmurray@liverpoolmathsschool.org>
2025-03-12 21:10:23 +0000

    populate and improve packages

--- src/Capabilities.tsx
+++ src/Capabilities.tsx
@@ -1,6 +1,4 @@
-import baseSatellite from "./assets/baseSatellite.png";
 import droneImage from "./assets/parrotAnafi.png";
-import radarDish from "./assets/radarDish.png";
 
 function Capabilities() {
   return (
--- src/Packages.tsx
+++ src/Packages.tsx
@@ -16,7 +16,16 @@ function Packages() {
               <h5 className="packagesText">Baseline Intelligence</h5>
             </div>
             <hr></hr>
-            <h6>test</h6>
+            <ul className="included">
+              <li>High level threat assessments</li>
+              <li>Refined publicly available intelligence reports</li>
+              <li>Basic access to secure communication</li>
+            </ul>
+            <ul className="notIncluded">
+              <li>Direct operational support</li>
+              <li>Classified or proprietary intelligence</li>
+              <li>Custom or tailored surveillance</li>
+            </ul>
 
         </SpotlightCard>
         <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
@@ -24,6 +33,18 @@ function Packages() {
               <h5 className="packagesText">Advanced Intelligence</h5>
             </div>
             <hr></hr>
+            <ul className="included">
+              <li>Real-time situational intelligence</li>
+              <li>Access to restricted-source intelligence</li>
+              <li>Bespoke risk mitigation reports</li>
+              <li>Counterintelligence assessments</li>
+              <li>Limited operational advisories</li>
+            </ul>
+            <ul className="notIncluded">
+              <li>Full-spectrum asset deployment</li>
+              <li>Direct kinetic support</li>
+              <li>Unrestricted access to classified intelligence</li>
+            </ul>
 
         </SpotlightCard>
         <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(77, 77, 77, 0.5)">
@@ -31,7 +52,18 @@ function Packages() {
               <h5 className="packagesText">Elite & Covert Operations Support</h5>
             </div>
             <hr></hr>
-
+            <ul className="included">
+              <li>Covert operational intelligence</li>
+              <li>Classified strategic briefings</li>
+              <li>Advanced counterintelligence and cybersecurity assessments</li>
+              <li>Direct liaison with specialized assets</li>
+              <li>Deep reconnaissance and threat analysis</li>
+            </ul>
+            <ul className="notIncluded">
+              <li>Public acknowledgment or transparency</li>
+              <li>Unlimited operational intervention</li>
+              <li>Any records of engagement</li>
+            </ul>
         </SpotlightCard>
       </div>
     </div>
