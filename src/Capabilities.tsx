c0b3fed745b7ee587db770162179b468bff1338b
RoryMurray7 <24rmurray@liverpoolmathsschool.org>
2025-03-13 14:47:36 +0000

    Begin privacy policy

--- src/Capabilities.tsx
+++ src/Capabilities.tsx
@@ -36,29 +36,6 @@ function Capabilities() {
           </div>
         </div>
       </div>
-
-      {/* <div className="capabilitiesCardContainer">
-        <div className="capabilitiesCard">
-          <img className="capabilitiesCardImage" src={droneImage}></img>
-          <p className="imageTitle">Aerial Reconnaissance</p>
-        </div>
-
-        <div className="capabilitiesCard">
-          <img className="capabilitiesCardImage" src={baseSatellite}></img>
-          <p className="imageTitle">
-            Satellite<br></br>Imagery
-          </p>
-        </div>
-
-        <div className="capabilitiesCard">
-          <img className="capabilitiesCardImage" src={radarDish}></img>
-          <p className="imageTitle">Radar & Signal Monitoring</p>
-        </div>
-
-        <div className="capabilitiesCard">
-          <div className="capabilitiesCardImage"></div>
-        </div>
-      </div> */}
     </div>
   );
 }
--- src/Privacypolicy.tsx
+++ src/Privacypolicy.tsx
@@ -2,8 +2,11 @@ import "./index.css";
 
 function Privacypolicy() {
   return (
-    <div id="termsOfServiceContainer">
-      <h1>Someone needs to write a load of waffle here</h1>
+    <div id="termsOfServiceContainer" className="center">
+              <h1 style={{marginBottom: "-2rem"}}>Privacy Policy</h1>
+      <h6 style={{color: "grey"}}>Last Updated: 13/03/2025</h6>
+      <h4>1. Governing Law</h4>
+      <p>This article is governed by the following UK laws:</p>
     </div>
   );
 }
